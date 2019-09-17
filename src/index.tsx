4e35a767f7eb320266ba2a41506f0a11d95fcc23
Ibrahim Saberi <georgeipsum@gmail.com>
2019-09-17 17:58:00 -0400

    added rose function

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default function rose(blue: any) {
+  console.log(blue);
+  
+  ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  serviceWorker.unregister();
+}
+
+const blue = "Welcome to zombocom";
+
+rose(blue);
