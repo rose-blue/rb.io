b8e98fbc4c25725217c9190eb4d5ca4056cc0113
Ibrahim Saberi <georgeipsum@gmail.com>
2019-09-23 20:09:52 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Hero from './components/hero';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppProps = {
+
+};
+
+type AppState = {
+
+};
+
+class App extends React.Component<AppProps, AppState> {
+  render() {
+    return (
+      <div className="App">
+        <div className="Hero-container">
+          <Hero />
+        </div>
+        <div className="Roseblue Left">
+          <div className="container">
+            <div className="buoy">
+              <div className="clip"></div>
+            </div>
+            <h1>rose(blue)</h1>
+          </div>
+        </div>
+        <div className="Mast-text">
+          <div className="container">
+            <div className="text-container">
+              <h1>designed to communicate,<br/>engineered to perform</h1>
+              <p>test</p>
+              <p>arrow</p>
+            </div>
+          </div>
+        </div>
+        <div className="Roseblue Right">
+          <div className="buoy">
+            <div className="clip"></div>
+          </div>
+          <div className="container">
+            <div className="nav">
+              nav
+            </div>
+          </div>
+        </div>
+        <div style={{height: 3000}}></div>
+        <svg width="0" height="0">
+          <defs>
+            <clipPath id="right">
+              <path d="M0,48c14.1,0,27.8-12.2,40-24S65.9,0,80,0V48Z"/>
+            </clipPath>
+            <clipPath id="left">
+              <path d="M80,0C65.9,0,52.2,12.2,40,24S14.1,48,0,48V0Z"/>
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+    );
+  }
 }
 
 export default App;
