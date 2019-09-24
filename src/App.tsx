e791d63ae1244d709bc9a7eed95749153c2148fe
Ibrahim Saberi <georgeipsum@gmail.com>
2019-09-24 15:22:18 -0400

    home page change

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import './App.scss';
 import Hero from './components/hero';
+import Button from './components/button';
+
+import { scroll } from './utilities/scroll';
 
 type AppProps = {
 
 };
 
 type AppState = {
-
+  scroll: any
 };
 
 class App extends React.Component<AppProps, AppState> {
+  componentDidMount() {
+    const SCROLL = scroll('a[href*="#"]', {
+      speed: 750,
+      speedAsDuration: true,
+      easing: 'easeInOutQuint'
+    });
+    this.state = {
+      scroll: SCROLL
+    };
+  }
+
   render() {
     return (
       <div className="App">
+        {window.innerWidth > 6000 ? (<div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: 'black', zIndex: 1000}}>
+          <div style={{display: 'flex', height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center', alignContent: 'center'}}><h1>Hey! What the fuck is your problem.</h1></div>
+        </div>) : null}
         <div className="Hero-container">
           <Hero />
         </div>
@@ -29,12 +46,13 @@ class App extends React.Component<AppProps, AppState> {
           <div className="container">
             <div className="text-container">
               <h1>designed to communicate,<br/>engineered to perform</h1>
-              <p>test</p>
-              <p>arrow</p>
+              <a data-scroll href="#testi">
+                <Button style={{marginTop: '2rem'}}>learn how</Button>
+              </a>
             </div>
           </div>
         </div>
-        <div className="Roseblue Right">
+        <div id="testi" className="Roseblue Right">
           <div className="buoy">
             <div className="clip"></div>
           </div>
@@ -42,6 +60,11 @@ class App extends React.Component<AppProps, AppState> {
             <div className="nav">
               nav
             </div>
+          </div>
+        </div>
+        <div className="testi">
+          <div className="container">
+            <h2><span style={{fontWeight: 300}}>rose(blue)</span> is a design team focused on human-centered online experiences.<br/>We develop digital solutions to fulfill your practical needs and creative dreams.</h2>
           </div>
         </div>
         <div style={{height: 3000}}></div>
